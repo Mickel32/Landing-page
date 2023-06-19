d44d2b8d6746edb4990fa2230bd93d8e251aebc4
Mickel32 <mlimaico@icloud.com>
2023-06-19 17:07:52 -0500

    responsive sass

--- JavaScript/responsive.js
+++ JavaScript/responsive.js
@@ -1,16 +1,11 @@
 let ancho = document.documentElement.clientWidth;
 const bannerpromo= document.getElementById("bannerpromo");
-const bannerplan = document.getElementById("container-plan");
-const distancia = bannerplan.style.gap;
 
 
 window.addEventListener('resize', function(resize){
     bannerpromo.style.width=ancho;
     var anchura = bannerpromo.offsetWidth;
     bannerpromo.style.height= (anchura / 2.9) + "px";
-    if (ancho < 1700 + "px"){
-        bannerplan.style.gap = distancia - (1 + "rem");
-    }
 })
 
 window.onload = function(){
